171d17e14215d73d3064ff1dd41d48a87820b913
SemereGr <semere.grt1@hotmail.com>
2026-03-05 18:12:39 +0800

    modify gitstuff

--- lua/plugins/gitstuff.lua
+++ lua/plugins/gitstuff.lua
@@ -54,15 +54,7 @@ return {
 		},
 		-- setting up with keys={} allows plugin to load when command runs at the start
 		keys = {
-			-- { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
-			{
-				"<leader>lg",
-				function()
-					vim.system({ "bash", "-c", "ssh-add ~/.ssh/id_ed25519_git && lazygit" }, { detach = true })
-					vim.cmd("LazyGit")
-				end,
-				desc = "Open lazy git (SSH)",
-			},
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
 		},
 	},
 }
